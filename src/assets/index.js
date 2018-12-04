6f13e1aacf2489dbcd771ec2c67d0537d4d03440
Tim Humphries <thumphries@cartrawler.com>
2018-12-04 18:56:11 +0000

    add bracelets, rings and pendants images

---
+++ src/assets/index.js
@@ -0,0 +1,56 @@
+export const assets = {
+  main: require('./main.jpg'),
+  logo: require('./logo.png'),
+
+  ring1: require('./ring1.jpeg'),
+  ring2: require('./ring2.jpeg'),
+  ring3: require('./ring3.jpeg'),
+  ring4: require('./ring4.jpeg'),
+  ring5: require('./ring5.jpeg'),
+  ring6: require('./ring6.jpeg'),
+  ring7: require('./ring7.jpeg'),
+
+  pendant1: require('./pendant1.jpeg'),
+  pendant2: require('./pendant2.jpeg'),
+  pendant3: require('./pendant3.jpeg'),
+  pendant4: require('./pendant4.jpeg'),
+  pendant5: require('./pendant5.jpeg'),
+  pendant6: require('./pendant6.jpeg'),
+
+  bracelet1: require('./bracelet1.jpeg'),
+  bracelet2: require('./bracelet2.jpeg'),
+  bracelet3: require('./bracelet3.jpeg'),
+  bracelet4: require('./bracelet4.jpeg'),
+  bracelet5: require('./bracelet5.jpeg'),
+  chain1: require('./chain1.jpeg'),
+  chain2: require('./chain2.jpeg'),
+};
+
+export const ringsAssets = [
+  assets.ring1,
+  assets.ring2,
+  assets.ring3,
+  assets.ring4,
+  assets.ring5,
+  assets.ring6,
+  assets.ring7,
+];
+
+export const pendantsAssets = [
+  assets.pendant1,
+  assets.pendant2,
+  assets.pendant3,
+  assets.pendant4,
+  assets.pendant5,
+  assets.pendant6,
+];
+
+export const braceletsChainsAssets = [
+  assets.bracelet1,
+  assets.bracelet2,
+  assets.bracelet3,
+  assets.bracelet4,
+  assets.bracelet5,
+  assets.chain1,
+  assets.chain2,
+];
