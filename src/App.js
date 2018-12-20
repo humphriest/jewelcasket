b8e33b58f39e17d3d2fca2e61e6ad87906014c9a
Tim Humphries <thumphries@cartrawler.com>
2018-12-20 11:19:23 +0000

    remove flow for now

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-// @flow
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import store from './redux/store';
 
+import store from './redux/store';
 import AppViewContainer from './AppViewContainer';
 
 class App extends Component {
