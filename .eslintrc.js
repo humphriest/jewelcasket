d1f94dc8416802782a31280839afe5da37835167
Tim Humphries <thumphries@cartrawler.com>
2019-01-11 17:21:18 +0000

    move server and add redux logger and update thunk

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,18 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react'],
+  rules: {
+    'no-console': 'off',
+    'comma-dangle': ['error', 'only-multiline'],
+    'arrow-parens': ['error', 'as-needed'],
+    'function-paren-newline': 'off',
+    'quote-props': ['error', 'consistent-as-needed'],
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '_|ignore',
+        varsIgnorePattern: '_|ignore',
+      },
+    ],
+  },
 };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const routes = require('./src/api/routes/api');
+const routes = require('./api/routes/api');
 const path = require('path');
 require('dotenv').config();
 
